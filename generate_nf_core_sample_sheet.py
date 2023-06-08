dae38e032970111036e4c603c3f67703af6c1d79
ChrisEisenhartGilead <119362377+ChrisEisenhartGilead@users.noreply.github.com>
2023-06-08 10:45:54 -0500

    Adding more documentation and better handling of corner cases

--- generate_nf_core_sample_sheet.py
+++ generate_nf_core_sample_sheet.py
@@ -6,23 +6,24 @@ Generate sample sheets for Gilead nf-core pipelines from an AWS s3 bucket.  It i
 will attempt to identify the samples present, and create a samplesheet that can be passed into
 the corresponding Gilead nf-core pipeline.
 """
-import os
 import sys
 import argparse
 import logging
 import subprocess
-import re
 
 log = None  # The global logger is defined, it will be initiated after the user commands are read
 
 
 def validate_options(options):
     """
-    Use this to enable conditional mandatory arguments (ex Sarek)
+    Use this to perform additional checks on parameters and to enable conditional mandatory arguments (ex Sarek)
     """
+    if options.s3_bucket.startswith("s3"):
+        print("The S3 bucket should not include 's3:/', it should be format /my_bucket/part1/part2/")
+        exit(1)
     if options.pipeline == "sarek":
         if options.sarek_status is None:
-            print("The --sarek_type option is required for the 'sarek' pipeline.")
+            print("The --sarek_status option is required for the 'sarek' pipeline.")
             exit(1)
         if options.sarek_status == "paired":
             if options.sarek_mapping_file is None:
@@ -87,7 +88,7 @@ def get_s3_fastq_files(s3_directory):
     """
     Takes an S3 directory and returns a list of the fastq files that are present.
     """
-    # Execute the aws s3 ls --recursive command
+    # Execute the aws s3 ls --recursive command    
     command = f"aws s3 ls s3:/{s3_directory} --recursive"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     # Check if the command was successful
@@ -113,13 +114,14 @@ def parse_sample_data(s3_path):
     and returns a sample name.
     SH8611_SA146502D_S63
     """
-
+    # Find the index of the lane element of the fastq file if it exists (ex; L001)
     def find_lane_index(elements):
         for i, element in enumerate(elements):
             if element.startswith("L") and len(element) == 4 and element[1:].isdigit():
                 return i
         return None
 
+    # Find the index of the orientation element of the fastq file (R1 or R2)
     def find_orient_index(elements):
         for i, element in enumerate(elements):
             if element.startswith("R") and len(element) == 2 and element[1:].isdigit():
@@ -140,6 +142,26 @@ def parse_sample_data(s3_path):
         orient_index = find_orient_index(sub_components)
         sample_name = "_".join(sub_components[:orient_index])
         lane = ""
+
+    # Look for Fulgent specific patterns ex; FT-SA204671R-FT-SPN04508_H5VHMDSX7 or FT-SPN04508_H5VHMDSX7-FT-SA204671R
+    for comp in components:
+        if "FT-SA" in comp: # This identifies a Fulgent Sample accession
+            acc_loc = comp.index("FT-SA") # Identify where in this component we will see the accession
+            sub_components = comp.split("-")
+            cur_loc = 0
+            found_acc = False
+            ft_sample_accession = ""
+            for sub_comp in sub_components:
+                if found_acc:
+                    ft_sample_accession = sub_comp
+                    assert ft_sample_accession.startswith("SA") # Verify at the correct component
+                    break
+                if cur_loc == acc_loc: # Reached the 'FT' prior to the Sample Accession in the string
+                    found_acc = True
+                cur_loc += len(sub_comp) + 1
+            sample_name = sub_comp
+            break
+
     return sample_name, lane
 
 
@@ -174,7 +196,7 @@ def pair_fastq_file_list(fastq_file_list):
         return paired_files
 
 
-def generate_nf_core_rnaseq_sample_sheet(paired_files):
+def generate_nf_core_rnaseq_sample_sheet(paired_files, s3_bucket):
     """
     sample,fastq_1,fastq_2,strandedness
     SAMPLE_PE,SAMPLE_PE_RUN1_1.fastq.gz,SAMPLE_PE_RUN1_2.fastq.gz,forward
@@ -182,14 +204,20 @@ def generate_nf_core_rnaseq_sample_sheet(paired_files):
     SAMPLE_SE,SAMPLE_SE_RUN1_1.fastq.gz,,forward
     """
     results = ["sample,fastq_1,fastq_2,strandedness"]
+    s3_base = s3_bucket.split("/")[1]
     for data in paired_files:
         sample_name, lane, r1_fastq, r2_fastq = data
-        sample_sheet_entry = f"{sample_name},{r1_fastq},{r2_fastq},auto"
+        sample_sheet_entry = f"{sample_name},s3://{s3_base}/{r1_fastq},s3://{s3_base}/{r2_fastq},auto"
         results.append(sample_sheet_entry)
     return results
 
 
 def get_sample_name_to_status(mapping_file, paired_files):
+    """
+    Parse user provided mapping file to generate a sample mapping dict which
+    maps sample names to their status ex; {"SA204434": "germline"}
+    """
+    # Dict to map the sample name to the sample status (tumor/germline)
     sample_name_to_status = {}
     with open(mapping_file, "r") as file:
         count = 0
@@ -224,7 +252,7 @@ def get_sample_name_to_status(mapping_file, paired_files):
     return sample_name_to_status
 
 
-def generate_nf_core_sarek_sample_sheet(paired_files, in_status, mapping_file):
+def generate_nf_core_sarek_sample_sheet(paired_files, s3_bucket, in_status, mapping_file):
     """
     patient,sex,status,sample,lane,fastq_1,fastq_2
     patient1,XX,0,normal_sample,lane_1,test_L001_1.fastq.gz,test_L001_2.fastq.gz
@@ -232,6 +260,7 @@ def generate_nf_core_sarek_sample_sheet(paired_files, in_status, mapping_file):
     patient2,NA,0,normal_sample,lane_1,test_L003_1.fastq.gz,test_L003_2.fastq.gz
     """
     results = ["patient,sex,status,sample,lane,fastq_1,fastq_2"]
+    s3_base = s3_bucket.split("/")[1]
     if in_status == "paired":
         sample_name_to_status = get_sample_name_to_status(mapping_file, paired_files)
     status = 1 if in_status == "tumor" else 0
@@ -240,7 +269,7 @@ def generate_nf_core_sarek_sample_sheet(paired_files, in_status, mapping_file):
         if in_status == "paired":
             status = sample_name_to_status[sample_name]
         sample_sheet_entry = (
-            f"{sample_name},NA,{status},{sample_name},{lane},{r1_fastq},{r2_fastq}"
+            f"{sample_name},NA,{status},{sample_name},{lane},s3://{s3_base}/{r1_fastq},s3://{s3_base}/{r2_fastq}"
         )
         results.append(sample_sheet_entry)
     return results
@@ -254,12 +283,12 @@ def main(args):
     if options.pipeline == "rnaseq":
         fastq_files = get_s3_fastq_files(options.s3_bucket)
         paired_files = pair_fastq_file_list(fastq_files)
-        output_data = generate_nf_core_rnaseq_sample_sheet(paired_files)
+        output_data = generate_nf_core_rnaseq_sample_sheet(paired_files, options.s3_bucket)
     elif options.pipeline == "sarek":
         fastq_files = get_s3_fastq_files(options.s3_bucket)
         paired_files = pair_fastq_file_list(fastq_files)
         output_data = generate_nf_core_sarek_sample_sheet(
-            paired_files, options.sarek_status, options.sarek_mapping_file
+            paired_files, options.s3_bucket, options.sarek_status, options.sarek_mapping_file
         )
     with open(options.outFile, "w") as out_file:
         out_file.write("\n".join(output_data))
